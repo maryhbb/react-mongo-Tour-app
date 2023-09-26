f14672990fae51defb45dafb1597d0defe2ce99e
Mary <76174247+maryhbb@users.noreply.github.com>
2023-09-26 13:26:35 +0200

    Implement Create Form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -3,11 +3,21 @@ import Place from "@/db/models/Place";
 
 export default async function handler(request, response) {
   await dbConnect();
-
   if (request.method === "GET") {
     const places = await Place.find();
-    return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+    response.status(200).json(places);
+  }
+
+  try {
+    if (request.method === "POST") {
+      const places = request.body;
+      await Place.create(places);
+      response.status(201).json({ status: "You're Place was created." });
+    }
+  } catch (error) {
+    console.error(error);
+    response
+      .status(400)
+      .json({ status: `Something went wrong: ${error.message}` });
   }
 }
